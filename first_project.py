a195a35c99f642d716cc3c90ce7f579896826c0f
romam <romamihnev714@gmail.com>
2024-10-11 21:50:48 +0300

    HW_2.2_from_11.10.2024

--- first_project.py
+++ first_project.py
@@ -2,14 +2,29 @@
 
 
 
-number = int(input("Enter number:"))
-n1 = number // 1000
-n2 = number // 100 % 10
-n3 = number % 100 // 10
-n4 = number % 10
-print(n1)
-print(n2)
-print(n3)
-print(n4)
+# number = int(input("Enter number:"))
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number % 100 // 10
+# n4 = number % 10
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+
+
+number = int(input('Enter a number:'))
+n1 = number // 10000
+n2 = number //  1000 % 10
+n3 = number % 1000 // 100
+n4 = number % 100 // 10
+n5 = number % 10
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+# print(n5)
+result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+print(result)
 
 
