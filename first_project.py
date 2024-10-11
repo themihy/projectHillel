dc63293f269796aaca2dd410cb32b4d4ee936a3d
romam <romamihnev714@gmail.com>
2024-10-11 21:35:46 +0300

    HW_2.1_from_11.10.2024

--- first_project.py
+++ first_project.py
@@ -1,4 +1,15 @@
-print("hello world")
-print("how are you")
-print("prosto")
+# print("hello world")
+
+
+
+number = int(input("Enter number:"))
+n1 = number // 1000
+n2 = number // 100 % 10
+n3 = number % 100 // 10
+n4 = number % 10
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+
 
