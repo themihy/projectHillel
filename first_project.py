291edd2110504f3bd80d1994ce9aab161fc1c53f
romam <romamihnev714@gmail.com>
2024-10-12 09:01:26 +0300

    reinforcement_of_learned_11.10.2024

--- first_project.py
+++ first_project.py
@@ -1,6 +1,5 @@
 # print("hello world")
-
-
+from unittest import removeResult
 
 # number = int(input("Enter number:"))
 # n1 = number // 1000
@@ -13,18 +12,39 @@
 # print(n4)
 
 
-number = int(input('Enter a number:'))
-n1 = number // 10000
-n2 = number //  1000 % 10
-n3 = number % 1000 // 100
-n4 = number % 100 // 10
-n5 = number % 10
+#number = int(input('Enter a number:'))
+#n1 = number // 10000
+#n2 = number //  1000 % 10
+#n3 = number % 1000 // 100
+#n4 = number % 100 // 10
+#n5 = number % 10
 # print(n1)
 # print(n2)
 # print(n3)
 # print(n4)
 # print(n5)
-result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
-print(result)
+#result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+#print(result)
 
+#number = int(input("Enter your number"))
+#n1 = number // 10
+#n2 = number // 100
+#n3 = number // 1000
 
+#n1 = number // 10 %
+#n2 = number % 100 // 10
+#n3 = number % 1000 // 100
+
+#print(n1)
+#print(n2)
+#print (n3)
+
+number_a = 28
+number_b = 41
+result = number_a * number_b
+print (result)
+
+first_number = int(input("Enter a first number:"))
+second_number = int(input("Enter a second number"))
+result = first_number + second_number
+print(f"Hello, your lacky number is {result}")
