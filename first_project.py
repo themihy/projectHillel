33d99d98e133f3b5c7254366d7a34b643cdad5f4
romam <romamihnev714@gmail.com>
2024-11-02 10:47:16 +0200

    hw_frоm_29.10.2024

--- first_project.py
+++ first_project.py
@@ -443,3 +443,48 @@ from unicodedata import numeric
 # # Тест
 # assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
 # print('ОК')
+
+
+
+
+
+
+#hw8.1
+# def add_one(some_list):
+#     # Об'єднуємо цифри в рядок, перетворюємо в число, додаємо 1
+#     number = int(''.join(map(str, some_list))) + 1
+#     # Перетворюємо результат назад у список цифр
+#     return [int(digit) for digit in str(number)]
+#
+# # Тестування функції
+# assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+# assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+# assert add_one([0]) == [1], 'Test3'
+# assert add_one([9]) == [1, 0], 'Test4'
+# print("ОК")
+
+#hw8.2
+# def is_palindrome(text):
+#     # Оставляем только буквы и цифры, приводим к нижнему регистру
+#     cleaned_text = ''.join(c.lower() for c in text if c.isalnum())
+#     # Проверяем, является ли очищенный текст палиндромом
+#     return cleaned_text == cleaned_text[::-1]
+#
+# # Тестирование функции
+# assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+# assert is_palindrome('0P') == False, 'Test2'
+# assert is_palindrome('a.') == True, 'Test3'
+# assert is_palindrome('aurora') == False, 'Test4'
+# print("ОК")
+
+#hw8.3
+# def find_unique_value(some_list):
+#     for num in some_list:
+#         if some_list.count(num) == 1:
+#             return num
+#
+# # Тестування функції
+# assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+# assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+# assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+# print("ОК")
