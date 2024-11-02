246f9cc30d051cce62571ec2aeaa484e59c0b0e7
romam <romamihnev714@gmail.com>
2024-11-02 22:11:14 +0200

    hw_frоm_29.10.2024

--- first_project.py
+++ first_project.py
@@ -450,20 +450,7 @@ from unicodedata import numeric
 
 
 #hw8.1
-#v1
-# def add_one(some_list):
-#     # Об'єднуємо цифри в рядок, перетворюємо в число, додаємо 1
-#     number = int(''.join(map(str, some_list))) + 1
-#     # Перетворюємо результат назад у список цифр
-#     return [int(digit) for digit in str(number)]
-#
-# # Тестування функції
-# assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
-# assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
-# assert add_one([0]) == [1], 'Test3'
-# assert add_one([9]) == [1, 0], 'Test4'
-# print("ОК")
-#v2
+
 # def add_one(some_list):
 #     # Перетворюємо список цифр на число
 #     number = 0
