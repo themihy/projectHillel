0d03660b143cbf83186cf6c729dfe762ecf80616
romam <romamihnev714@gmail.com>
2024-11-14 22:38:12 +0200

    hw_frоm_09.11.2024

--- first_project.py
+++ first_project.py
@@ -18,20 +18,20 @@
 #     print(grivna / 44)
 # else:
 #     print("Eror")
-while True:
-    temp = int(input("Enter a temp: "))
-    if temp <= -26 or temp >= 70:
-        print("Erorr, temepature is a so big for person")
-        continue
-    elif temp == -26 or temp < 5:
-        print("Cold")
-    elif temp == 6 or temp < 25:
-        print("Normal")
-    elif temp == 26 or temp < 50:
-        print("Hot")
-    else:
-        print("Danger")
-        break
+# while True:
+#     temp = int(input("Enter a temp: "))
+#     if temp <= -26 or temp >= 70:
+#         print("Erorr, temepature is a so big for person")
+#         continue
+#     elif temp == -26 or temp < 5:
+#         print("Cold")
+#     elif temp == 6 or temp < 25:
+#         print("Normal")
+#     elif temp == 26 or temp < 50:
+#         print("Hot")
+#     else:
+#         print("Danger")
+#         break
 # number = int(input("Enter number:"))
 # n1 = number // 1000
 # n2 = number // 100 % 10
@@ -636,3 +636,59 @@ while True:
 # assert is_even(0) == True, 'Test3'
 # print('OK')
 
+
+
+
+#hw11.1
+# def prime_generator(end):
+#     # Проходимо по всіх числах від 2 до end (включно)
+#     for num in range(2, end + 1):
+#         # Перевіряємо, чи число просте
+#         is_prime = True
+#         for i in range(2, int(num ** 0.5) + 1):  # Перевіряємо до квадратного кореня з num
+#             if num % i == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime:
+#             yield num  # Повертаємо просте число
+#
+# # Тести
+# from inspect import isgenerator
+#
+# gen = prime_generator(1)
+# assert isgenerator(gen) == True, 'Test0'
+# assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+# assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+# assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+# print('Ok')
+#
+
+
+#hw11.2
+# def generate_cube_numbers(end):
+#     n = 2
+#     while n**3 < end:
+#         yield n**3  # Повертаємо куб числа
+#         n += 1
+#     return  # Завершуємо генератор, коли куб числа перевищує end
+#
+# # Тести
+# from inspect import isgenerator
+#
+# gen = generate_cube_numbers(1)
+# assert isgenerator(gen) == True, 'Test0'
+# assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+# assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+# assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+# print('Ok')
+
+#hw11.3
+# def is_even(number):
+#     return (number & 1) == 0
+#
+# # Тести
+# assert is_even(2494563894038**2) == True, 'Test1'
+# assert is_even(1056897**2) == False, 'Test2'
+# assert is_even(24945638940387**3) == False, 'Test3'
+#
+# print("Ok")
