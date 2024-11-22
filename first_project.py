8cde804dd37db270db9e42f994a14015071322ff
romam <romamihnev714@gmail.com>
2024-11-22 06:57:46 +0200

    hw_frоm_16.11.2024

--- first_project.py
+++ first_project.py
@@ -799,3 +799,147 @@
 #
 # # Перевірка оновленої вартості
 # assert cart.get_total() == 80, 'Всього 80'
+
+
+
+#hw13.1
+# class Human:
+#     def __init__(self, gender, age, first_name, last_name):
+#         self.gender = gender
+#         self.age = age
+#         self.first_name = first_name
+#         self.last_name = last_name
+#
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}, {self.gender}, {self.age} years old"
+#
+# class Student(Human):
+#     def __init__(self, gender, age, first_name, last_name, record_book):
+#         super().__init__(gender, age, first_name, last_name)
+#         self.record_book = record_book
+#
+#     def __str__(self):
+#         return f"Student: {super().__str__()}, Record Book: {self.record_book}"
+#
+# class Group:
+#     def __init__(self, number):
+#         self.number = number
+#         self.group = set()
+#
+#     def add_student(self, student):
+#         self.group.add(student)
+#
+#     def delete_student(self, last_name):
+#         student = self.find_student(last_name)
+#         if student:
+#             self.group.remove(student)
+#
+#     def find_student(self, last_name):
+#         for student in self.group:
+#             if student.last_name == last_name:
+#                 return student
+#         return None
+#
+#     def __str__(self):
+#         all_students = "\n".join(str(student) for student in self.group)
+#         return f"Group Number: {self.number}\n{all_students}"
+#
+# # Тестування
+#
+# st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+# st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+# gr = Group('PD1')
+# gr.add_student(st1)
+# gr.add_student(st2)
+# print(gr)
+#
+# assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+# assert gr.find_student('Jobs2') is None, 'Test2'
+# assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+#
+# gr.delete_student('Taylor')
+# print(gr)  # Only one student
+#
+# gr.delete_student('Taylor')  # No error!
+
+
+#hw13.2
+# class Counter:
+#     def __init__(self, current=1, min_value=0, max_value=10):
+#         # Ініціалізація атрибутів поточного значення, мінімального та максимального значення
+#         self.current = current
+#         self.min_value = min_value
+#         self.max_value = max_value
+#
+#     def set_current(self, start):
+#         """Встановлює поточне значення лічильника"""
+#         if self.min_value <= start <= self.max_value:
+#             self.current = start
+#         else:
+#             raise ValueError(f"Current value must be between {self.min_value} and {self.max_value}.")
+#
+#     def set_max(self, max_max):
+#         """Встановлює максимальне значення лічильника"""
+#         self.max_value = max_max
+#         if self.current > self.max_value:
+#             self.current = self.max_value
+#
+#     def set_min(self, min_min):
+#         """Встановлює мінімальне значення лічильника"""
+#         self.min_value = min_min
+#         if self.current < self.min_value:
+#             self.current = self.min_value
+#
+#     def step_up(self):
+#         """Збільшує поточне значення лічильника на 1, викидаючи виняток, якщо досягнуто максимум"""
+#         if self.current < self.max_value:
+#             self.current += 1
+#         else:
+#             raise ValueError("Reached maximum value.")
+#
+#     def step_down(self):
+#         """Зменшує поточне значення лічильника на 1, викидаючи виняток, якщо досягнуто мінімум"""
+#         if self.current > self.min_value:
+#             self.current -= 1
+#         else:
+#             raise ValueError("Reached minimum value.")
+#
+#     def get_current(self):
+#         """Повертає поточне значення лічильника"""
+#         return self.current
+#
+# # Тестування
+# counter = Counter()
+#
+# # Встановлюємо поточне значення 7
+# counter.set_current(7)
+#
+# # Збільшуємо лічильник на 3
+# counter.step_up()
+# counter.step_up()
+# counter.step_up()
+# assert counter.get_current() == 10, 'Test1'
+#
+# # Тепер намагаємося перевищити максимальне значення
+# try:
+#     counter.step_up()  # Очікується виняток
+# except ValueError as e:
+#     print(e)  # Достигнут максимум
+# assert counter.get_current() == 10, 'Test2'
+#
+# # Встановлюємо мінімум на 7
+# counter.set_min(7)
+#
+# # Тепер зменшуємо лічильник, поки не досягнемо мінімуму
+# counter.step_down()
+# counter.step_down()
+# counter.step_down()
+# assert counter.get_current() == 7, 'Test3'
+#
+# # Тепер намагаємося зменшити лічильник нижче мінімуму
+# try:
+#     counter.step_down()  # Очікується виняток
+# except ValueError as e:
+#     print(e)  # Достигнут минимум
+# assert counter.get_current() == 7, 'Test4'
+
